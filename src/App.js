7e1f26436d7a4a94ac3f4d21339c99ef1d6f44c5
jakdev047 <shuvo047niter@gmail.com>
2020-04-11 17:52:31 +0600

    to add react lazy function

--- src/App.js
+++ src/App.js
@@ -1,45 +1,45 @@
-import React,{ useState } from 'react';
+import React,{ useState,lazy,Suspense} from 'react';
 import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
 import './assets/css/style.css';
-// components
-import Navbar from './components/Navbar/Navbar';
-import Cart from './components/Cart/Cart';
-
-// pages
-import Home from './views/Home';
-import Checkout from './views/Checkout';
-import ProductDetails from './views/ProductDetails';
-import Error from './views/Error';
 
 // context
 import ThemeContext  from './context/ThemeContext';
 import { StateProvider } from './store';
 
+// components
+import Navbar from './components/Navbar/Navbar';
+
+// pages
+import Error from './views/Error';
+const Home = lazy(() => import('./views/Home'));
+const Checkout = lazy(() => import('./views/Checkout'));
+const ProductDetails = lazy(() => import('./views/ProductDetails'));
+const Cart = lazy(() => import('./components/Cart/Cart'));
+
 const App = () => {
   const [dark,setdark] = useState(false);
-
   const toggleDark = () => {
     setdark(isDark=>!isDark);
   }
-
   return (
     <StateProvider>
       <ThemeContext.Provider value={{dark:dark,toggle:toggleDark}}>
         <div className="App">
             <Router>
               <Navbar/>
-              <Switch>
-                <Route exact path="/" component={Home}/>
-                <Route exact path="/product/:id" component={ProductDetails} />
-                <Route exact path="/checkout" component={Checkout} />
-                <Route path="*" component={Error} />
-              </Switch>
-              <Cart />
+              <Suspense fallback={<div><h2>Loading...</h2></div>}>
+                <Switch>
+                  <Route exact path="/" component={Home}/>
+                  <Route exact path="/product/:id" component={ProductDetails} />
+                  <Route exact path="/checkout" component={Checkout} />
+                  <Route path="*" component={Error} />
+                </Switch>
+                <Cart />
+              </Suspense>
             </Router>
         </div>
       </ThemeContext.Provider>
     </StateProvider>
   );
 }
-
 export default App;
