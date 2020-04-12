c6ca3c78e4220caf6a5354c652e8e4e1d4fe39c1
jakdev047 <shuvo047niter@gmail.com>
2020-04-12 20:03:32 +0600

    api use of productList component

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -1,7 +1,6 @@
 import React,{useEffect} from 'react';
 import ProductItem from './ProductItem';
 import { useCart } from '../../customHooks/useCart';
-import data from '../../data/data';
 import { useSelector, useDispatch } from 'react-redux';
 import {setProducts} from '../../action';
 
@@ -11,10 +10,14 @@ const ProductList = () => {
   const dispatch = useDispatch();
 
   useEffect(()=>{
-    const results = data.filter(product => 
-      product.title.includes(keyword) || product.brand.includes(keyword)
-    )
-    dispatch(setProducts(results));
+    fetch(`https://murmuring-fortress-41915.herokuapp.com/products?keyword=${keyword}`)
+    .then(res=>res.json())
+    .then(data=> {
+      const results = data.filter(product => 
+        product.title.includes(keyword) || product.brand.includes(keyword)
+      )
+      dispatch(setProducts(results));
+    })
   },[dispatch,keyword]);
   return (
     <div className="product-list">
